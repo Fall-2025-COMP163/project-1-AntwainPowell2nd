c8983bac074504c1ce10228aae98f03f97dab86f
AntwainPowell2nd <antwain.powell.2nd@gmail.com>
2025-10-29 17:12:57 -0400

    commot 21

--- project1_starter.py
+++ project1_starter.py
@@ -79,8 +79,8 @@ def calculate_stats(character_class, level):
 def save_character(character, filename):
     with open(filename, 'w') as file:
         for key, value in character.items():
-            if key == "Name":
-                file.write(f"Character {key}: {value}\n")
+            if key.lower() == "name":
+                file.write(f"Character Name : {value}\n")
                 continue
             file.write(f"{key}: {value}\n")
     return True
